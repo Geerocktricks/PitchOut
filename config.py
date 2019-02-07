82d9e10d787e1270052d0de4c8c8a3c7996beee9
Geerocktricks <geerockface4@gmail.com>
2019-02-07 11:42:06 +0300

    This commit sets up the app __init__.py file with some imports and initializations

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 
 class Config:
 
-    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
