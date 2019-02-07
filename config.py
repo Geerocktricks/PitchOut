e2acececdaf3758fb9cfbdc61e10aba4c69ed779
Geerocktricks <geerockface4@gmail.com>
2019-02-07 16:21:27 +0300

    This commit adds the configuration URI

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
 
 class Config:
-
+        """
+    This is the parent class which will have the general configurations
+    """
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://geerocktricks:Geerock_1@localhost/PitchOut'
 
 
 class ProdConfig(Config):
