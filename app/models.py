b844eb735dabe77343be731dbda425617aa208f2
Geerocktricks <geerockface4@gmail.com>
2019-02-08 17:44:45 +0300

    This commit installs and initializes the flask_migrate

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer , primary_key = True)
     username = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return f'User {self.username} '
@@ -19,6 +20,7 @@ class Role(db.Model):
 
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
 
     def __repr__(self):
         return f'User {self.name}'
--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server, Shell
-from app.models import User
+from app.models import User,Role
+from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = create_app('development')
@@ -9,7 +10,7 @@ manager = Manager(app)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User , Role = Role )
 manager.add_command('server',Server)
 
 manager.add_command('server',Server)
