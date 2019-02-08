ed2e44ce505ce319306a83a9b07f4738d16cce1d
Geerocktricks <geerockface4@gmail.com>
2019-02-08 08:04:32 +0300

    This commit creates the flask script shell in the manage.py

--- manage.py
+++ manage.py
@@ -1,10 +1,15 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server, Shell
+from app.models import User
 
 # Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
 manager.add_command('server',Server)
 
 manager.add_command('server',Server)
