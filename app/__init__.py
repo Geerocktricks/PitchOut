841989c25d8525aff5715a40c066556782c5b8b7
Geerocktricks <geerockface4@gmail.com>
2019-02-08 11:13:40 +0300

    This commit creates the index, macros and base templates

--- app/__init__.py
+++ app/__init__.py
@@ -27,7 +27,7 @@ def create_app(config_name):
     # Will add the views and forms
 
     # setting config
-    from .requests import configure_request
-    configure_request(app)
-git 
+    # from .requests import configure_request
+    # configure_request(app)
+# git 
     return app
--- config.py
+++ config.py
@@ -1,12 +1,16 @@
 import os
 
 class Config:
-        """
+    """
     This is the parent class which will have the general configurations
     """
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://geerocktricks:Geerock_1@localhost/PitchOut'
+
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 class ProdConfig(Config):
