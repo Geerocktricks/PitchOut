46062362e45a5d2f98e0c848de4313fcd77104d1
Geerocktricks <geerockface4@gmail.com>
2019-02-08 17:27:00 +0300

    This commit creates the role class

--- app/models.py
+++ app/models.py
@@ -1,9 +1,24 @@
 from . import db
 
 class User(db.Model):
+    '''
+    user class to help create new users
+    '''
     __tablename__ = 'users'
     id = db.Column(db.Integer , primary_key = True)
     username = db.Column(db.String(255))
 
     def __repr__(self):
         return f'User {self.username} '
+
+class Role(db.Model):
+    '''
+    Role class to give users different roles and access
+    '''
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.name}'
