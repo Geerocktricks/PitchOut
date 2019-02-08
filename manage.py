f42ceb1779884667ef928511573b06f36c36d041
Geerocktricks <geerockface4@gmail.com>
2019-02-08 21:48:50 +0300

    This commit creates flask migrations

--- manage.py
+++ manage.py
@@ -13,6 +13,9 @@ def make_shell_context():
     return dict(app = app,db = db,User = User , Role = Role )
 manager.add_command('server',Server)
 
+migrate = Migrate(app,db)
+manager.add_command("db", MigrateCommand)
+
 manager.add_command('server',Server)
 if __name__ == '__main__':
     manager.run()
