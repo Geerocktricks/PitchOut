6e3b28ab2368794ac874c4e34875afce503bbc05
Geerocktricks <geerockface4@gmail.com>
2019-02-09 17:23:17 +0300

    This commit takes care of the login and the logout functionality

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 from wtforms import ValidationError
+
 
 
 class RegistrationForm(FlaskForm):
@@ -19,3 +20,10 @@ class RegistrationForm(FlaskForm):
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is taken')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,15 +1,27 @@
 from . import auth
 from flask import render_template
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for ,flash,request
+from flask_login import login_user
 from ..models import User
-from .forms import RegistrationForm
+from .forms import LoginForm,RegistrationForm
 from .. import db
+from flask_login import login_user,logout_user,login_required
 
 
 
-@auth.route('/login')
+@auth.route('/login' ,methods=['GET','POST'])
 def login():
-    return render_template('auth/login.html')
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "PitchOut login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
 
 
 @auth.route('/register',methods = ["GET","POST"])
@@ -22,3 +34,10 @@ def register():
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
