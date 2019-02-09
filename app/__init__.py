2ae8c86f3e9cf5200970e3b589a2bbcc15515c7b
Geerocktricks <geerockface4@gmail.com>
2019-02-09 13:07:38 +0300

    This commit installs flask_login

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,10 @@ def create_app(config_name):
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    def create_app(config_name):
+    #...
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
 
     # Creating the app configurations
