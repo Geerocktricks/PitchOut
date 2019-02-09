466938758e27d65f31957e82239f68b5411cce01
Geerocktricks <geerockface4@gmail.com>
2019-02-09 16:42:18 +0300

    This commit creates the forms.py file for new user registration

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,12 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask import Blueprint
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 
 auth = Blueprint('auth',__name__)
 
@@ -18,8 +24,7 @@ def create_app(config_name):
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-    def create_app(config_name):
-    #...
+    
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
@@ -32,6 +37,7 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     # Will add the views and forms
 
--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,12 @@
 from flask import render_template
 from .import main
+from flask_login import login_required
 #views
 @main.route('/')
 def index():
     '''Index method to display the main index page
     '''
     name = "PitchOut"
+    title = "PitchOut"
 
-    return render_template('index.html' , name = name)
+    return render_template('index.html' , name = name , title = title)
