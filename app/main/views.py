ae817f1a4b1a73b520dd028e68411c14fab26893
Geerocktricks <geerockface4@gmail.com>
2019-02-10 13:15:08 +0300

    This commit adds the profile.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
 from .import main
 from flask_login import login_required
+from ..models import User
 #views
 @main.route('/')
 def index():
@@ -8,5 +9,16 @@ def index():
     '''
     name = "PitchOut"
     title = "PitchOut"
+    
 
-    return render_template('index.html' , name = name , title = title)
+    return render_template('index.html' , name = name , title = title )
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
--- app/models.py
+++ app/models.py
@@ -17,7 +17,9 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    pass_secure  = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
 
     @property
     def password(self):
