eb15236a24429a8607538b07e59d6e67ed396ef8
Geerocktricks <geerockface4@gmail.com>
2019-02-10 13:37:33 +0300

    This commit installs flask-uploads

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,9 @@ from flask import render_template,request,redirect,url_for,abort
 from .import main
 from flask_login import login_required
 from ..models import User
+from .forms import UpdateProfile
+from .. import db
+
 #views
 @main.route('/')
 def index():
@@ -22,3 +25,23 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user)
+
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
