cae004f23f6bf3b6067642926f66c72e93496a2c
Geerocktricks <geerockface4@gmail.com>
2019-02-12 11:48:01 +0300

    deployment to heroku

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from app.models import User,Role,Pitch, Comment
 from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 migrate = Migrate(app,db)
@@ -12,12 +12,7 @@ migrate = Migrate(app,db)
 manager.add_command('server',Server)
 manager.add_command('db',MigrateCommand)
 
-@manager.command
-def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+
 
 @manager.shell
 def make_shell_context():
