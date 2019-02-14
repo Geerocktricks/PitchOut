0f1dc7e8e403a84f7acaeba01cc01479587336a6
Geerocktricks <geerockface4@gmail.com>
2019-02-14 15:47:42 +0300

    pushing to heroku 2nd time

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ class Config:
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_BLACK_URL")
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://geerocktricks:Geerock_1@localhost/pitchout_test'
